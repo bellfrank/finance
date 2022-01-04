d133f8c076d1127e777b42c0dba0d8cf9bd0d36f
Macbook Pro <joserodriguez@Joses-MacBook-Pro.local>
2022-01-04 13:50:44 -0800

    update

--- app.py
+++ app.py
@@ -35,6 +35,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
+#db = SQL(os.getenv("DATABASE_URL"))
 db = SQL(os.getenv("DATABASE_URL"))
 
 # Make sure API key is set
